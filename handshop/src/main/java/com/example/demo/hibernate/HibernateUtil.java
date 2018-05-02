347804de2774ac25241946aa19ed9a014ab57448
lehanhtri287 <lehanhtri287@gmail.com>
2018-05-02 14:19:33 +0700

    Created web spring boot

---
+++ handshop/src/main/java/com/example/demo/hibernate/HibernateUtil.java
@@ -0,0 +1,32 @@
+package com.example.demo.hibernate;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+@SuppressWarnings("deprecation")
+public class HibernateUtil {
+	private static final SessionFactory session = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() {
+		try {
+			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+					.configure("hibernate.cfg.xml")
+					.build();
+			Metadata metadata = new MetadataSources(serviceRegistry)
+					.getMetadataBuilder()
+					.build();
+			return metadata.getSessionFactoryBuilder().build();
+		} catch (Throwable ex) {
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return session;
+	}
+
+}
